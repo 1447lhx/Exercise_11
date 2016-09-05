bbf575b4823a39d7d48a554f65ccc7b809de605e
1447lhx <1959142791@qq.com>
2016-09-05 22:38:52 +0800

    Initial commit

---
+++ app/src/main/java/cn/edu/bistu/cs/cnse/edu/bistu/cs/se/exercise_11/MainActivity.java
@@ -0,0 +1,42 @@
+package cn.edu.bistu.cs.cnse.edu.bistu.cs.se.exercise_11;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button = (Button) findViewById(R.id.Second);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,Main2Activity.class);
+                intent.putExtra("name", "lihuixai");
+                intent.putExtra("number", 2014011389);
+                startActivityForResult(intent, 0);
+            }
+        });
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 0 && resultCode == 0) {
+            String str = data.getStringExtra("result");
+            Toast.makeText(this, str, Toast.LENGTH_LONG).show();
+        }
+
+    }
+}
